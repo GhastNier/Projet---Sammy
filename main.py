3b7a245420d3b1042a6d34ae38187703b4c336fa
Michael Savard <michael.savardgelinas@mail.mcgill.ca>
2022-09-28 00:06:49 -0400

    Revert Merge CSV_Add

--- main.py
+++ main.py
@@ -13,32 +13,37 @@ csvOut = r'cdr_processed.csv'
 jsonPath = r'data.json'
 
 
-def csv_to_json(csvInbound, csvOutbound, jsonFilePath):
+def csv_to_json(csvDocPath, jsonFilePath):
     jsonArray = []
 
-    with open(csvInbound, encoding='utf-8') as csvf:
-        data = pd.read_csv(csvf,
-                           usecols=['Date', 'Account', 'CallerID', 'Destination', 'Description', 'Seconds', 'Rate',
-                                    'Total', 'UniqueID'],
-                           dtype={'Date': str, 'Time': str, 'Account': str,
-                                  'CallerID': str, 'Destination': str,
-                                  'Description': str,
-                                  'Seconds': str, 'Rate': str, 'Total': str,
-                                  'UniqueID': str})
+    with open(csvDocPath, encoding='utf-8') as csvf:
+        csvReader = csv.DictReader(csvf)
 
-    data[['Date', 'Time']] = data.Date.str.split(expand=True, )
-    data.to_csv(csvOutbound,
-                columns=['Date', 'Time', 'Account', 'CallerID', 'Destination', 'Description', 'Seconds', 'Rate',
-                         'Total', 'UniqueID'], index=False)
-
-    for row in csvOutbound:
-        jsonArray.append(row)
+        for row in csvReader:
+            jsonArray.append(row)
 
     with open(jsonFilePath, 'w', encoding='utf-8') as jsonf:
         jsonString = json.dumps(jsonArray, indent=4)
         jsonf.write(jsonString)
 
 
+def csv_add_column(csvInbound, csvOutbound):
+    data = pd.read_csv(csvInbound,
+                       usecols=['Date', 'Account', 'CallerID', 'Destination', 'Description', 'Seconds', 'Rate',
+                                'Total', 'UniqueID'],
+                       dtype={'Date': str, 'Time': str, 'Account': str,
+                              'CallerID': str, 'Destination': str,
+                              'Description': str,
+                              'Seconds': str, 'Rate': str, 'Total': str,
+                              'UniqueID': str})
+
+    data[['Date', 'Time']] = data.Date.str.split(expand=True, )
+    data.to_csv(csvOutbound,
+                columns=['Date', 'Time', 'Account', 'CallerID', 'Destination', 'Description', 'Seconds', 'Rate',
+                         'Total', 'UniqueID'], index=False)
+
+
 if __name__ == '__main__':
     print_hi('PyCharm')
-    csv_to_json(csvIn, csvOut, jsonPath)
+    csv_add_column(csvIn, csvOut)
+    csv_to_json(csvOut, jsonPath)
